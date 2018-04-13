b33a79d4d8b9b0eae771550d77b23fe4da2b083e
Ryanbritt <britt.ryant@gmail.com>
2018-04-13 08:24:18 -0400

    Working on the messages component

--- client/components/matchComponents/OneMatch.js
+++ client/components/matchComponents/OneMatch.js
@@ -10,18 +10,60 @@ export default class OneMatch extends React.Component {
       userToMessage: 7,
       newMessageBody: "",
       messageHistory: null,
-      messagesReceived: false
+      messagesReceived: false,
+      username: this.props.personData.username,
+      description: this.props.personData.description,
+      renderMessages: false
     }
     // this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleNavigation = this.handleNavigation.bind(this)
   }
   componentDidMount(){
-    services.getAllMessages(this.state)
-    .then(results => {
-      this.setState({
-        messagesReceived: true,
-        messageHistory: results.data.data
-      })
+    console.log(`I am the props for the OneMatch Component`, this.props, "And I am the state!", this.state);
+    this.setState({
+      apiDataLoaded: true
     })
+    // services.getAllMessages(this.state)
+    // .then(results => {
+    //   this.setState({
+    //     messagesReceived: true,
+    //     messageHistory: results.data.data
+    //   })
+    // })
   }
-  renderData
+  handleNavigation(){
+    console.log(`I am the handleNavigation function and the message button was clicked!`);
+  }
+  whatToRender(){
+    return  <View>{this.state.renderMessages ? this.renderMessageScreen() : this.renderInfo()}</View>
+  }
+
+  renderMessageScreen(){
+    return(
+      <View>
+        <Text>I will be the messages Component</Text>
+      </View>
+    )
+  }
+
+  renderInfo(){
+    return(
+      <View>
+        <Text>{this.state.description}</Text>
+      </View>
+    )
+  }
+
+  render(){
+    return(
+      <View>
+        <Text>{this.state.username}</Text>
+        {this.state.apiDataLoaded ? this.whatToRender() : ''}
+        <Button
+          title={this.state.renderMessages ? `See ${this.state.username}'s info` : `Message ${this.state.username}`}
+          onPress={() => this.setState({renderMessages: !this.state.renderMessages})}
+        />
+      </View>
+    )
+  }
 }
